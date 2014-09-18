1ef4c1b06ed86e3d876c0c2937474d914016089a
Tony Fast <tony.fast@gmail.com>
2014-09-18 14:48:26 -0400

    Python function to access github content through links

---
+++ rpages.py
@@ -0,0 +1,62 @@
+def github( url, service = '' ):
+    from urlparse import urlsplit
+
+    if service == 'api':
+        data = req_to_pd( url = url, force_json=True )
+
+    elif service =='repo':
+
+        data =  req_to_pd( url = 'https://api.github.com/repos' +\
+                                urlsplit( url ).path,
+                    force_json=True )
+    elif service == 'history':
+        parsed = urlsplit( url )
+        parts = parsed.path.lstrip('/').split('/')
+        for ct, piece in enumerate(parts):
+            if piece == 'blob' or piece=='tree':
+                break
+
+        repo = ('/').join(parts[0:2])
+        sha = parts[ct+1]
+        path =  ('/').join(parts[ct+2:])
+
+        data =  req_to_pd( url = 'https://api.github.com/repos/' +\
+                                repo + '/commits?path=' + path +\
+                                '&sha=' + sha,
+                    force_json=True, typ = 'frame', show_url=True )
+
+    elif '/blob/' in url or service =='blob' or service == 'fm':
+        url = url.replace( '/blob','')
+        parsed = urlsplit( url )
+        parsed = parsed._replace(netloc='raw.githubusercontent.com')
+        if service == 'fm':
+            data = frontmatter.load(n).metadata
+        else:
+            data = req_to_pd( parsed.geturl() )
+
+    elif '/commit' in url or service=='noembed':
+        data = noembed( url )
+
+    return data
+
+def req_to_pd( url , typ='series', force_json=False, show_url=False, has_fm=False):
+
+    import requests
+    import pandas as pd
+    import yaml
+    from urlparse import urlsplit
+
+    if show_url:
+        print url
+
+    parsed = urlsplit( url )
+    s = requests.get(  url )
+
+    if parsed.path.endswith('.json') or force_json:
+        data = pd.io.json.read_json( s.text, typ = typ )
+    elif parsed.path.endswith('.yml') or parsed.path.endswith('.yaml'):
+        data = yaml.load( s.text )
+    else:
+        data = s.text
+
+    return data
